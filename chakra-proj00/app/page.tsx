eb6082c2bd9bb4a62b530a33cda6d21b710dfb7d
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2023-01-15 01:13:38 +0500

    chakra is working now with nextjs13

--- chakra-proj00/app/page.tsx
+++ chakra-proj00/app/page.tsx
@@ -1,23 +1,27 @@
 'use client'
 import { ChakraProvider } from '@chakra-ui/react'
 import { extendTheme } from '@chakra-ui/react'
-import {Flex,Heading,Input, Button,useColorMode, useColorModeValue}  from '@chakra-ui/react'
+import {Flex,Heading,Input, Button, useColorMode,useColorModeValue}  from '@chakra-ui/react'
 
 
 export default function Home() {
-  const {toggleColorMode}= useColorMode()
-  const formBackground = useColorModeValue("gray.100","gray.700")
+  const { colorMode, toggleColorMode } = useColorMode()
+  const formBackground = useColorModeValue("gray.100","gray.700");
   return (
     <div>
+      <ChakraProvider>
       <Flex height="100vh" alignItems="center"  justifyContent="center">
       <Flex direction="column" background={formBackground} p={12} rounded={6}>
         <Heading mb={6}>Log in</Heading>
         <Input placeholder='abc@xyz.com' variant="filled" mb={3} type="email"/>
         <Input placeholder='*******' variant="filled" mb={6} type="password"/>
         <Button mb={6} colorScheme="teal">Login</Button>
-        <Button onClick={toggleColorMode}>Toggle color</Button>
+        
+        <Button onClick={toggleColorMode}>Toggle {colorMode === 'light' ? 'Dark' : 'Light'}</Button>
       </Flex>
     </Flex>
+    </ChakraProvider>
+
     </div>
     
     // <ChakraProvider>
